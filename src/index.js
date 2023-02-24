93cd9d4d97f41183ea5815c166bdeb615e728a1c
Andrey Soroko <andreysoroko84@gmail.com>
2023-02-24 08:43:32 +0300

    update task

--- src/index.js
+++ src/index.js
@@ -1,51 +1,61 @@
 module.exports = function check(str, bracketsConfig) {
-  let openSymbol = '';
-  let closeSymbol = '';
-  let result;
+    
+    var stack = [];
+    let incorrectOrder;
+    
+    for (let i = 0; i < str.length; i++) {
+        let add = false;
+        let del = false;
 
-  for (let i = 0; i < bracketsConfig.length; i++) {
-      openSymbol = bracketsConfig[i][0];
-      closeSymbol = bracketsConfig[i][1];
-          // console.log(`Open symbol: ${openSymbol}\nClose symbol:${closeSymbol}`);
-          // console.log(checkString(str, openSymbol, closeSymbol));
-              if (str.includes(openSymbol) && str.includes(closeSymbol)) {
-                  result =  checkString(str, openSymbol, closeSymbol);
-              };
+        if (stack.length === 0) {
+            bracketsConfig.forEach(element => {
+                if (str[i] === element[0] && stack.length === 0) {
+                    add = true; 
+                }
 
-          if (!result) return false;
-      }
-  return(result);
+                if (str[i] === element[1] && element[0] !== element[1]) incorrectOrder = true;
+            });
+
+        } else { // if (stack.length !== 0)
+
+            bracketsConfig.forEach(element => {
+                // console.log('str[i] :',str[i]);
+
+                if (str[i] === element[0] && str[i] === element[1]) { // If open and close brackets is equal (element[0] === element[1])
+                    if (stack.at(-1) === str[i]) {
+                        del = true; //console.log('delete equal bracket');
+                    } else {
+                        add = true; //console.log('add equal bracket');
+                    }
+
+                } else { //if element[0] !== element[1]
+                    if (str[i] === element[0] ) {
+                        // console.log(`Add ${str[i]}`);
+                        add = true;
+                    }
+                    if (stack.at(-1) === element[0] && str[i] === element[1]) {
+                        // console.log(`Del ${str[i]}`);
+                        del = true;
+                    }
+                }
+            });
+
+        }
+        
+        if (add) stack.push(str[i]); //Add
+        if (del) stack.pop(str[i]);
+    } // for
+    
+    if (incorrectOrder) {
+        // console.log('Incorrect order of brackets');
+        return false;
+    }
+    if (stack.length === 0) {
+        // console.log(`Stack is empty.`);
+        return true;
+    } else {
+        // console.log(`Stack is NOT empty. Length: ${stack.length} Stack: ${stack}`);
+        return false;
+    }
+
 }
-
-function checkString(str, openSymbol, closeSymbol) {
-  let strIsOpen;
-  let strIsClose;
-
-  for (let i = 0; i < str.length; i++) {
-      if (str[i] === openSymbol) {
-          strIsOpen = true;
-          strIsClose = false;
-      }
-      if (str[i] === closeSymbol && strIsOpen) {
-          strIsOpen = false;
-          strIsClose = true;
-      }
-  }
-  if (strIsClose) {
-      let openSymbol1 = closeSymbol;
-      let closeSymbol1 = openSymbol;
-
-      for (let i = 0; i < str.length; i++) {
-          if (str[i] === openSymbol1) {
-              strIsOpen = true;
-              strIsClose = false;
-          }
-          if (str[i] === closeSymbol1 && strIsOpen) {
-              strIsOpen = false;
-              strIsClose = true;
-          }
-      }
-  }
-  return strIsClose;
-}// function checkString
-
